69ab479d281188f5bf0237e14eed82053dcb4d7e
JsooAh <joo1798@naver.com>
2024-07-19 10:40:41 +0900

    init : 백엔드 초기 파일 설ì정

---
+++ backend/src/test/java/com/ssafy/tarotbom/TarotbomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tarotbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotbomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
