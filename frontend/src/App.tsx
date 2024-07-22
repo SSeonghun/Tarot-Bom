4189d802ce0b7bf7f429e965f2e04ae427ab68a2
unknown <gje960217@naver.com>
2024-07-22 09:59:59 +0900

    fix/폴더와 파일 이름 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Main';
+import Home from './pages/Home';
 
 import Login from './pages/UserAuth/Signin';
 import Navbar from './components/Navbar';
