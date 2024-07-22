d659340ebfa7e0111655933c19e57d430e8ab406
JsooAh <joo1798@naver.com>
2024-07-22 16:51:44 +0900

    feat: 타로 상담방 관련 Entity 구축

---
+++ backend/src/main/java/com/ssafy/tarotbom/domain/room/entity/TarotLog.java
@@ -0,0 +1,42 @@
+package com.ssafy.tarotbom.domain.room.entity;
+
+import com.ssafy.tarotbom.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="tarot_log")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class TarotLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id", columnDefinition = "int unsigned")
+    private long logId;
+
+    /* 유저ID, 방Id : @ManyToOne */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member userId;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room roomId;
+    
+    @Column(name = "type", columnDefinition = "int(1)")
+    private int type;
+    
+    @Column(name = "time", columnDefinition = "timestamp")
+    private LocalDateTime time;
+    
+    // 튜플 삽입시 출입시간 자동으로 기록되도록
+    @PrePersist
+    public void prePersist() {
+        this.time = LocalDateTime.now();
+    }
+}
