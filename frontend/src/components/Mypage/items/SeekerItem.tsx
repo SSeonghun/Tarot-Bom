381ca5c9292247ce3e5dcd7f2b111e41bf89de40
최현석 <smrtnrlgh@naver.com>
2024-07-25 17:56:36 +0900

    feat : 마이페이지 프로필 이미지 - 프로필 이미지 - 마이페이지 섹션 나누기

---
+++ frontend/src/components/Mypage/items/SeekerItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ReaderMypage: React.FC = () => {
+  return (
+    <div className="container p-4 mx-auto">
+      <div>
+        <h1 className="text-black font-bold text-[60px]">
+          김싸피님 마이페이지
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default ReaderMypage;
