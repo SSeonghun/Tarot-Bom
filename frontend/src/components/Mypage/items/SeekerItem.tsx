67e735a882d4bcdd1673b23c8302a246645a5ab2
최현석 <smrtnrlgh@naver.com>
2024-07-26 17:48:24 +0900

    feat: 마이페이지 설계

--- frontend/src/components/Mypage/items/SeekerItem.tsx
+++ frontend/src/components/Mypage/items/SeekerItem.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import HoverButton from "../../Common/HoverButton";
 
 const ReaderMypage: React.FC = () => {
   return (
     <div className="container p-4 mx-auto">
-      <div>
-        <h1 className="text-black font-bold text-[60px]">
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-black font-bold text-[50px] m-4">
           김싸피님 마이페이지
         </h1>
+        <div className="m-4">
+          <HoverButton
+            label="리더 프로필 만들기"
+            color="bg-gray-300"
+            hoverColor="bg-gray-500"
+            hsize="h-12"
+            wsize="w-48"
+            fontsize="text-lg"
+          ></HoverButton>
+        </div>
       </div>
+      <hr className="border-black" />
     </div>
   );
 };
---
+++ frontend/src/components/Mypage/items/item/Hero1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero1: React.FC = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-12">
+        <div className="grid-cols-4 bg-white"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero1;
