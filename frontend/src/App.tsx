715c3b659d3ed8b1c9e2852eb710d52c4f329ec3
ImSeongHun <daltjdgns111@naver.com>
2024-07-26 17:10:50 +0900

    feat: 타로결과 페이지 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import FindPwd from './pages/UserAuth/FindPwd';
 import ReaderProfile from './pages/UserProfile/ReaderProfle';
 import PostDetail from './components/Community/PostDetail';
 import CreatePostPage from './pages/Community/CreatePostPage';
+import TarotResult from './pages/TarotResult/TarotResult';
 
 const App: React.FC = () => {
   return (
@@ -35,6 +36,7 @@ const App: React.FC = () => {
           <Route path="/reader-profile" element={<ReaderProfile />} />
           <Route path="/posts/:id" element={<PostDetail />} />
           <Route path="/create-post" element={<CreatePostPage />} />
+          <Route path="/tarot-result" element={<TarotResult />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/TarotResult/TarotResult.tsx
+++ frontend/src/pages/TarotResult/TarotResult.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import resultBg from '../../assets/result-bg.jpeg'
+import img1 from '../../assets/Tarot_cards-removebg-preview.png'
 
-const SeekerMypage: React.FC = () => {
+const TarotResult: React.FC = () => {
   return (
-    <div className="container p-4 mx-auto">
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      <p>Welcome to the Home Page!</p>
+    <div className="relative w-screen min-h-screen bg-black">
+      {/* 배경 이미지 */}
+      <img src={resultBg} alt="result-background" className='absoulte inset-0 w-full h-screen' />
+
+      {/* 제목과 수평선 */}
+      <div className='absolute inset-0 flex justify-center items-start'>
+        <div className='flex flex-col items-center'>
+        <div className='flex flex-row items-center'>
+          <img src={img1} alt="img1" className=' w-1/3 ' />
+          <h1 className="text-6xl font-bold text-white">타로 결과</h1>
+        </div>
+        <hr className=' w-full border-white z-10' />
+      </div>
+      </div>
+       
     </div>
   );
 }
 
-export default SeekerMypage;
+export default TarotResult;
