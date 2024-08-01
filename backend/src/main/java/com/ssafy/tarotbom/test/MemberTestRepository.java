d55691cd091a54e01deb008f208b52a9dcba3dcc
JsooAh <joo1798@naver.com>
2024-08-01 17:19:18 +0900

    feat: S3 이미지 업로드 기능 구성

--- backend/src/main/java/com/ssafy/tarotbom/test/MemberTestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tarotbom.test;
-
-import com.ssafy.tarotbom.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberTestRepository extends JpaRepository<Member, Long> {
-
-}
--- backend/src/main/java/com/ssafy/tarotbom/test/ReaderTestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tarotbom.test;
-
-import com.ssafy.tarotbom.domain.member.entity.Reader;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReaderTestRepository extends JpaRepository<Reader, Long> {
-
-}
