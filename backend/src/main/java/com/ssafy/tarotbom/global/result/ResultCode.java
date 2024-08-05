cfdc06f05bb0afee96e4a448993664d177ae369d
최현석 <smrtnrlgh@naver.com>
2024-08-05 17:22:14 +0900

    fix: cors error

--- backend/src/main/java/com/ssafy/tarotbom/global/result/ResultCode.java
+++ backend/src/main/java/com/ssafy/tarotbom/global/result/ResultCode.java
@@ -20,11 +20,30 @@ public enum ResultCode {
     SIGNUP_OK(HttpStatus.CREATED, "M201", "회원가입 성공"),
     EMAIL_SEND_OK(HttpStatus.ACCEPTED, "M202", "이메일 전송"),
     VALIDATION_NUMBER_OK(HttpStatus.OK, "M004", "인증번호 인증 성공"),
+    READER_JOIN_OK(HttpStatus.CREATED, "M005", "리더 프로필 만들기 성공"),
+    CHANGE_READER_SEEKER_OK(HttpStatus.OK, "M006", "리더/시커 전환 성공"),
+    LOGOUT_OK(HttpStatus.OK , "M007", "로그아웃 성공"),
+
+    // Reader
+    SEARCH_ALL_READER(HttpStatus.OK, "E001", "전체 리더 조회 성공"),
+    SEARCH_READER_DETAIL(HttpStatus.OK, "E002", "리더 상세 조회 성공"),
+
+    // Favorite Reader
+    FAVORITE_READER_ADD(HttpStatus.CREATED, "F001", "리더 찜하기 완료"),
+    SEARCH_ALL_FAVORITE_READER(HttpStatus.OK, "F002", "찜한 리더 전체 조회"),
+    DELELTE_FAVORITE_READER(HttpStatus.OK, "F003", "찜한 리더 삭제 성공"),
+
+    // Mypage
+    SEARCH_SEEKER_MYPAGE(HttpStatus.OK, "Y001", "시커 마이페이지 조회"),
+    SEARCH_READER_MYPAGE(HttpStatus.OK, "Y002", "리더 마이페이지 조회"),
+
+
 
     // Tarot
     TAROT_CARD_INFO_FOUND(HttpStatus.OK, "T001", "카드 조회 성공"),
     TAROT_CARD_RESULT_SAVED(HttpStatus.CREATED, "T002", "타로 결과 저장 성공"),
     TAROT_CARD_RESULT_FOUND(HttpStatus.OK, "T003", "타로 결과 조회 성공"),
+
 
     ;
     // ==== 응답 코드 정의 종료 ====
