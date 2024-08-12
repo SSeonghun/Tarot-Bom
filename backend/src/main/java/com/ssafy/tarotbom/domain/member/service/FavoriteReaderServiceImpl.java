7c201794131272470f1bde41a380f2605d87cf7c
JsooAh <joo1798@naver.com>
2024-08-12 17:48:17 +0900

    fix: 찜한 리더 리스트에서 프로필 사진이 가져와지지 않던 현상 해결

--- backend/src/main/java/com/ssafy/tarotbom/domain/member/service/FavoriteReaderServiceImpl.java
+++ backend/src/main/java/com/ssafy/tarotbom/domain/member/service/FavoriteReaderServiceImpl.java
@@ -95,6 +95,7 @@ public class FavoriteReaderServiceImpl implements FavoriteReaderService{
                     .rating(reader.getRating())
                     .grade(reader.getGradeCode())
                     .price(reader.getPrice())
+                    .profileUrl(reader.getMember().getProfileUrl())
                     .build();
             favoriteReaderList.add(readerTemp);
         }
